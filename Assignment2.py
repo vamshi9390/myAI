4679d9bf03d9f6b3a2425735b5a9677a31d24c0b
vamshi9390 <kvamshi9390@gmail.com>
2019-06-04 23:44:26 +0530

    My second Assignment

---
+++ Assignment2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 10:01:26 2019
+
+@author: Vamshi Krishna
+"""
+game_answer=[9]
+guess_answer=[6]
+if (game_answer>guess_answer):
+    print('The answer is too low')
+else: (print('The answer is too high'))
+
+
+for  i in range(1,10):
+  multiply=5*i
+  if(multiply<=30):
+      print(multiply)
+      
+      
+      
+limit = 100
+num = 0 
+while (num +1)**2 < limit:
+    num += 1
+nearest_square = (num)**2
+
+
+print(nearest_square)
+    
+    
+    
